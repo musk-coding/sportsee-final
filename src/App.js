628e7d03e9eab9206e3f36083901036b4b5a1aaa
othmane <othmane.abouelyzza@gmail.com>
2021-04-25 11:36:59 +0200

    Clean code

--- src/App.js
+++ src/App.js
@@ -100,21 +100,3 @@ function App() {
 }
 
 export default App;
-
-// {
-//   "data": {
-//   "id": 18,
-//   "userInfos": {
-//   "firstName": "Cecilia",
-//   "lastName": "Ratorez",
-//   "age": 34
-//   },
-//   "score": 0.3,
-//   "keyData": {
-//   "calorieCount": 2500,
-//   "proteinCount": 90,
-//   "carbohydrateCount": 150,
-//   "lipidCount": 120
-//   }
-//   }
-//   }
